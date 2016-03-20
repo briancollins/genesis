4ec4223b6bcb3160b7a4edf1872719ea25d920d3
Brian Collins <bricollins@gmail.com>
2016-03-20 19:11:04 +0000

    Create an empty window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate piston;
+extern crate graphics;
+extern crate glutin_window;
+extern crate opengl_graphics;
+
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use piston::input::*;
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{ GlGraphics, OpenGL };
+
 fn main() {
-    println!("Hello, world!");
+    let opengl = OpenGL::V3_2;
+
+    // Create an Glutin window.
+    let mut window: Window = WindowSettings::new(
+            "genesis",
+            [1024, 768]
+        )
+        .opengl(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut events = window.events();
+    while let Some(e) = events.next(&mut window) {
+    }
 }
